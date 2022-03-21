6d6e1d5f779082092b69f9ac30391f5db260716c
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2022-03-21 14:25:16 +0100

    Templating update from 0.0.3

--- bash/run_pyspark_app.sh
+++ bash/run_pyspark_app.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 : '
-{{ WARNING_THIS_IS_TEMPLATED }}'
+#===================================================================================
+# THIS IS A TEMPLATED FILE; NEVER EDIT MANUALLY
+#
+# These configurations should not be modified here, they are handled by the Copier
+# tool and the DICE template. If you need to change any implementation in this file,
+# please consider if they are generic enough to be included in the template, or
+# use a project specific file for your needs. Instructions can be found in:
+# https://tools.adidas-group.com/bitbucket/projects/CG/repos/daa_eds_python_template
+#===================================================================================
+'
 
 # ...
